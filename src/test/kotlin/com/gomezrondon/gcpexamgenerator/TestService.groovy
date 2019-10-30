0de610b1471c74686639e10402dea5233aacc9b3
cajgomez <Jgomez@cambridgeassociates.com>
2019-10-30 11:20:01 -0600

    Adding 2 more custom questions.

--- src/test/kotlin/com/gomezrondon/gcpexamgenerator/TestService.groovy
+++ src/test/kotlin/com/gomezrondon/gcpexamgenerator/TestService.groovy
@@ -18,7 +18,7 @@ class TestService extends Specification {
         when:
         def variable = service.loadQuestions().size()
         then:
-        variable == 401
+        variable == 403
     }
 
 
